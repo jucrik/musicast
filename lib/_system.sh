17dda06609a100b021afc704db13aa58f18c31b2
jucrik <soporte@jucrik.com>
2023-08-03 14:59:43 -0500

    Radio

--- lib/_system.sh
+++ lib/_system.sh
@@ -12,7 +12,9 @@ system_copy_files() {
 
   sudo su - root <<EOF
   cd ~
-  cp /var/azuracast/radio-jucrik/img/hexbg.png /var/lib/docker/overlay2/${folder_azuracast}/diff/var/azuracast/www/web/static/img/hexbg.png
+  cp -r var/azuracast/radio-jucrik/img/ var/lib/docker/overlay2/${folder_azuracast}/diff/var/azuracast/www/web/static/img
+  sleep 2
+  rm -r var/azuracast/radio-jucrik
 EOF
 
   sleep 2
