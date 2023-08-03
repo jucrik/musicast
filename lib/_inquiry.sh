f4a1938fc45b87f063a0c6651965ce3c5159d9ae
jucrik <soporte@jucrik.com>
2023-08-03 15:53:28 -0500

    Radio

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -5,18 +5,7 @@
 folder_search() {
   print_banner
   printf "${YELLOW} 🔎 Buscando la carpeta codificada donde se instaló AzuraCast...${GRAY_LIGHT}"
-  wait_for("Presione Enter cuando haya copiado la carpeta para continuar", '\n')
-
-  sleep 2
-
-  sudo su - root <<EOF
-  cd ~
-  cp -r /var/azuracast/radio-jucrik/img/login/* /var/lib/docker/overlay2/${folder_azuracast}/diff/var/azuracast/www/web/static/img
-  sleep 2
-  cd ~ && rm -r /var/azuracast/radio-jucrik
-EOF
-
-  sleep 2
+  read -t
 }
 
 get_folder_azuracast() {
